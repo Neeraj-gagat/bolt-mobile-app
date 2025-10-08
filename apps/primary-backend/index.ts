74fd7ea1a70cfa981fc3fc86463d1fbde9015e52
Neeraj-gagat <neerajgagat9999@gmail.com>
2025-10-08 19:04:57 +0530

    added middleware

--- apps/primary-backend/index.ts
+++ apps/primary-backend/index.ts
@@ -1,19 +1,33 @@
 import {prismaClient} from  "db/client"
 import  express  from "express"
 import cors from "cors";
+import { authMiddleware } from "./middleware";
 
 const app = express();
 app.use(express.json());
 app.use(cors())
 
-app.post("/project", async (req, res) => {
-    const {prompt, ownerId} = req.body;
+app.post("/project", authMiddleware, async (req, res) => {
+    const {prompt} = req.body;
+    const userId = req.userId!;
 
-    const description = prompt.split("\n")[0]
+    const description = prompt.split("\n")[0];
     const project = await prismaClient.project.create({
-        data:{ description, ownerId }
+        data:{ description, ownerId:userId },
     });
     
     res.json({projectId: project.id})
 })
 
+app.get("/projects", authMiddleware, async (req, res) => {
+    const userId = req.userId!;
+    const project = await prismaClient.project.findMany({
+        where:{ ownerId:userId }
+    });
+    
+    res.json(project);
+})
+
+app.listen(3001, () => {
+    console.log("Server started on http://localhost:3001")
+})
---
+++ apps/primary-backend/middleware.ts
@@ -0,0 +1,26 @@
+import type { NextFunction, Response, Request } from "express";
+import  jwt  from "jsonwebtoken";
+
+export function authMiddleware(req: Request, res: Response, next:NextFunction) {
+    const token = req.headers.authorization;
+    if (!token) {
+        res.status(401).json({message: "missing token"})
+        return 
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_PUBLIC_KEY!);
+    if (!decoded) {
+        res.json(401).json({message: "Unauthorized token"})
+        return 
+    }
+
+    const userId = (decoded as any).payload.sub;
+    
+    if (!userId) {
+        res.json(401).json({message: "Unauthorized token"})
+        return
+    }
+
+    req.userId = userId;
+    next();
+}
