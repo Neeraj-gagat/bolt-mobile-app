a5334fb89e1ddfd56c334116bdedbb57ac20413a
Neeraj-gagat <neerajgagat9999@gmail.com>
2025-10-08 17:59:29 +0530

    project route

--- apps/primary-backend/index.ts
+++ apps/primary-backend/index.ts
@@ -1,1 +1,19 @@
 import {prismaClient} from  "db/client"
+import  express  from "express"
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.post("/project", async (req, res) => {
+    const {prompt, ownerId} = req.body;
+
+    const description = prompt.split("\n")[0]
+    const project = await prismaClient.project.create({
+        data:{ description, ownerId }
+    });
+    
+    res.json({projectId: project.id})
+})
+
