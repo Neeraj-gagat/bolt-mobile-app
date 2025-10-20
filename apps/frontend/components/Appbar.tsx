58f3e5058c9bafffb76d04b2ca52cf59f51adfae
Neeraj-gagat <neerajgagat9999@gmail.com>
2025-10-20 20:43:36 +0530

    minor ui fixes && sidebar fixed

--- apps/frontend/components/Appbar.tsx
+++ apps/frontend/components/Appbar.tsx
@@ -11,7 +11,7 @@ import {
 export function Appbar() {
     return <div className="bg-black w-full h-14 flex justify-between items-center px-8 border-b border-zinc-300/10 rounde">
         <p className="font-[700] text-[20px] cursor-pointer text-white">Boltify</p>
-        <div>
+        <div className="flex items-center">
         <SignedOut>
               <SignInButton />
               <SignUpButton>
